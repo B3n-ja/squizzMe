6617a803bbeb8818fb4e9f44bec56480675842b7
B3n-ja <robine.b@free.fr>
2018-07-19 15:53:36 +0200

    logout ok

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, App } from 'ionic-angular';
+
+import { AccueilPage } from '../accueil/accueil';
+
 
 @Component({
-  selector: 'page-settings',
-  templateUrl: 'settings.html'
+ selector: 'page-settings',
+ templateUrl: 'settings.html'
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController) {
+ constructor(public navCtrl: NavController, public appCtrl: App) {
 
-  }
+ }
+
+ logout() {
+  this.appCtrl.getRootNav().setRoot(AccueilPage);
+ }
 
 }
